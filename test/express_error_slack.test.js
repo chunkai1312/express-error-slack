a2667677f2ee66f84b7fe6e57c73ae61fb51fd95
Chun-Kai Wang <chunkai1312@gmail.com>
2017-01-22 04:25:11 +0800

    test(*): update

--- test/express_error_slack.test.js
+++ test/express_error_slack.test.js
@@ -1,15 +1,19 @@
-'use strict'
+import express from 'express'
+import Slack from 'slack-node'
+import createError from 'http-errors'
+import request from 'supertest'
+import { expect } from 'chai'
+import sinon from 'sinon'
+import errorToSlack from '../src/express_error_slack'
 
-const express = require('express')
-const Slack = require('slack-node')
-const createError = require('http-errors')
-const request = require('supertest')
-const expect = require('chai').expect
-const sinon = require('sinon')
-const errorToSlack = require('../')
+describe('Express Error Slack', () => {
+  const spy = sinon.spy(Slack.prototype, 'webhook')
 
-describe('Express Error Slack', function () {
-  it('should throw error if missing webhookUri', function () {
+  beforeEach(() => {
+    spy.reset()
+  })
+
+  it('should throw error if missing webhookUri', () => {
     const app = express()
     try {
       app.use(errorToSlack())
@@ -19,33 +23,35 @@ describe('Express Error Slack', function () {
     }
   })
 
-  it('should send error to slack for status code 4xx', function () {
+  it('should send error to slack for status code 4xx', (done) => {
     const app = express()
-    app.use(function (req, res, next) {
-      next(createError(500))
-    })
+    app.use((req, res, next) => next(createError(400)))
     app.use(errorToSlack({ webhookUri: 'https://hooks.slack.com/services/TOKEN' }))
 
-    return request(app)
-      .get('/')
-      .expect(500)
-      .end((err, res) => {
-        expect(err).to.exist
-      })
-  })
-
-  it('should send error to slack for status code 5xx', function () {
-    var app = express()
-    app.use(function (req, res, next) {
-      next(createError(400))
-    })
-    app.use(errorToSlack({ webhookUri: 'https://hooks.slack.com/services/TOKEN' }))
-
-    return request(app.listen())
+    request(app)
       .get('/')
       .expect(400)
       .end((err, res) => {
-        expect(err).to.exist
+        expect(err).to.not.exist
+        expect(spy.calledOnce).to.be.true
+        expect(spy.args[0][0].attachments[0]).to.have.property('color', 'warning')
+        done()
+      })
+  })
+
+  it('should send error to slack for status code 5xx', (done) => {
+    const app = express()
+    app.use((req, res, next) => next(createError(500)))
+    app.use(errorToSlack({ webhookUri: 'https://hooks.slack.com/services/TOKEN' }))
+
+    request(app)
+      .get('/')
+      .expect(500)
+      .end((err, res) => {
+        expect(err).to.not.exist
+        expect(spy.calledOnce).to.be.true
+        expect(spy.args[0][0].attachments[0]).to.have.property('color', 'danger')
+        done()
       })
   })
 })
