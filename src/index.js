4753623cbce6e5e4c6524a990479fe35907f8c3c
Chun-Kai Wang <chunkai1312@gmail.com>
2018-04-17 18:46:45 +0800

    fix: module export

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 import expressErrorSlack from './expressErrorSlack'
+import sendErrorToSlack from './sendErrorToSlack'
+
+expressErrorSlack.sendErrorToSlack = sendErrorToSlack
 
 export default expressErrorSlack
